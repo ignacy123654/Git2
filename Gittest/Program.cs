985faaf3feebaa4c538c4cfe99f0660d203fc711
AK <ignacy123654@gmail.com>
2019-10-07 21:13:33 +0200

    V1

---
+++ Gittest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gittest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            //4f6df701a4601fbd9dada6b7ab1955eebf1c4f23
+            //nowe cos
+            //cos nowego 4
+            //asdas
+            //zmiana kolejna
+            //zmiana 7
+
+        }
+    }
+}
